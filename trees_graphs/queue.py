cd625e19d803df6e9120172338c26f7ed8d30945
weixuanteo <weixuanteo@gmail.com>
2022-06-06 22:18:23 +0800

    Add queue
    Add queue implementation to tree_graphs folder

---
+++ trees_graphs/queue.py
@@ -0,0 +1,20 @@
+# Implementation of a queue using a linked list
+
+from singly_linked_list import SinglyLinkedList
+from node import Node
+
+class MyQueue:
+    def __init__(self):
+        self.queue = SinglyLinkedList()
+    
+    def enqueue(self, data):
+        self.queue.add_to_tail(data)
+    
+    def dequeue(self) -> Node:
+        return self.queue.remove_from_head()
+    
+    def peek(self) -> Node:
+        return self.queue.head
+    
+    def is_empty(self) -> bool:
+        return self.queue.is_empty()
