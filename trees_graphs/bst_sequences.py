f38d4d10163346bf8e4b338498493789e3ca1068
weixuanteo <weixuanteo@gmail.com>
2022-07-08 15:30:39 +0800

    Add tree and graphs 4.9

---
+++ trees_graphs/bst_sequences.py
@@ -0,0 +1,42 @@
+# A binary search tree was created by traversing through an array from left and right and inserting each element.
+# Given a binary search tree with distinct elements, print all possible arrays that could have led to it.
+# EXAMPLE
+# Input:
+#     2
+#    / \
+#   1   3
+# Output:
+# [2, 1, 3], [2, 3, 1]
+
+from tree_node import TreeNode
+
+def all_sequences(root: TreeNode) -> list:
+    if not root:
+        return []
+    return dfs(root)
+
+def dfs(root: TreeNode) -> list:
+    if not root:
+        return [[]]
+    left_seqs = dfs(root.left)
+    right_seqs = dfs(root.right)
+    seqs = []
+    for left in left_seqs:
+        for right in right_seqs:
+            seqs = weave(left, right, [root.val], seqs)
+    return seqs
+
+def weave(list1: list, list2: list, prefix: list, result: list) -> list[list]:
+    if not list1 and not list2:
+        result.append(prefix)
+    if list1:
+        result = weave(list1[1:], list2, prefix + [list1[0]], result)
+    if list2:
+        result = weave(list1, list2[1:], prefix + [list2[0]], result)
+    return result
+
+
+# root = TreeNode(2)
+# root.left = TreeNode(1)
+# root.right = TreeNode(3)
+# print(all_sequences(root))
---
+++ trees_graphs/tests/test_bst_sequences.py
@@ -0,0 +1,20 @@
+import unittest
+from bst_sequences import all_sequences, weave
+from tree_node import TreeNode
+
+class TestBSTSequences(unittest.TestCase):
+    def test_all_sequences(self):
+        tree1 = TreeNode(2)
+        tree1.left = TreeNode(1)
+        tree1.right = TreeNode(3)
+
+        expected1 = [[2, 1, 3], [2, 3, 1]]
+        result1 = all_sequences(tree1)
+        self.assertEqual(result1, expected1)
+
+    def test_weave(self):
+        l1 = [1, 2]
+        l2 = [3, 4]
+        expected = [[1,2,3,4], [1,3,2,4], [1,3,4,2], [3,1,2,4], [3,1,4,2], [3,4,1,2]]
+        result = weave(l1, l2, [], [])
+        self.assertEqual(result, expected)
